SDM710-Development/android_device_xiaomi_sdm710-common

sdm710-common: Add basic permission file for HotwordEnrollment
Change-Id: I2986ba3599fe65a8da27649d98d70cd548257aa8

sdm710.mk
@@ -59,6 +59,10 @@ PRODUCT_PACKAGES += \
     android.hidl.manager@1.0 \
     android.hidl.manager@1.0_system
 
+# HotwordEnrollement app permissions
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/permissions/privapp-permissions-hotword.xml:system/etc/permissions/privapp-permissions-hotword.xml
+
 # IFAA manager
 PRODUCT_PACKAGES += \
     org.ifaa.android.manager

VoltageOS-Devices/device_realme_sdm710-common

Signed-off-by: Harshit Jain <god@harsh1998.me>
Signed-off-by: improbir <ghosh_probir@yahoo.com>
Signed-off-by: MadhavSaladi <baaswanthmadhav@gmail.com>
Signed-off-by: KrishWalecha <Krishwalecha098@gmail.com>

sdm710.mk
@@ -260,6 +260,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/keylayout/gpio-keys.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/gpio-keys.kl
 
+# HotwordEnrollement app permissions
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/privapp-permissions-hotword.xml:system/etc/permissions/privapp-permissions-hotword.xml
+
 # IPACM
 PRODUCT_PACKAGES += \
     ipacm \
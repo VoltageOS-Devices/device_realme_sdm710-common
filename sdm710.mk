b6722a27b222b66b4ed402945fcfd99e475c3fe5
aleasto <ales.astone@gmail.com>
2022-08-03 13:44:26 +0530

    Build and enable fastbootd  * fastboot has issues while using USB 3 on RMX1851
    Change-Id: I0b20600fe7203a7aec19cbea8f6849052585c6ab

--- sdm710.mk
+++ sdm710.mk
@@ -255,6 +255,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/gps/izat.conf:$(TARGET_COPY_OUT_VENDOR)/etc/izat.conf \
     $(LOCAL_PATH)/configs/gps/sap.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sap.conf
 
+# fastbootd
+PRODUCT_PACKAGES += \
+    fastbootd
+
 # Health
 PRODUCT_PACKAGES += \
     android.hardware.health@2.1-impl:64 \

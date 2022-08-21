bf12e0c315c49b3c1d0b1f2faeb4d6a482c9e053
Cykeek <Cykeek@proton.me>
2022-08-21 13:30:00 +0530

    SDM710-common: Drop QTI thermal HAL * I dunno how to fix this so drop it
    Logs:
    08-20 15:27:46.138  1469  1469 I ThermalHalWrapper: Thermal HAL 2.0 service connected.
    08-20 15:27:46.138  1469  1469 E ThermalHalWrapper: Couldn't get temperatures because of HAL error: ThermalHAL not initialized properly.
    08-20 15:27:46.138  1469  1469 W ThermalManagerService: Thermal HAL reported invalid data, abort connection
    08-20 15:27:46.139  1469  1469 E ThermalHalWrapper: Couldn't get temperature thresholds because of HAL error: ThermalHAL not initialized properly.
    
    Signed-off-by: Cykeek <Cykeek@proton.me>

--- sdm710.mk
+++ sdm710.mk
@@ -452,10 +452,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     vendor.lineage.touch@1.0-service.RMX1851
 
-# Thermal
-PRODUCT_PACKAGES += \
-    android.hardware.thermal@2.0-service.qti
-
 # USB
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.0-service.basic

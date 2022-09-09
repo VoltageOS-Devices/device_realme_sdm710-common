3f36ca5a8df6c560730e3482c0020a069f2f7231
Cykeek <Cykeek@proton.me>
2022-09-09 15:41:00 +0530

    SDM710-common: Overlay: Add Bluetooth overlays
    Signed-off-by: Cykeek <Cykeek@proton.me>

--- sdm710.mk
+++ sdm710.mk
@@ -21,7 +21,8 @@ $(call inherit-product, frameworks/native/build/phone-xhdpi-4096-dalvik-heap.mk)
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay-lineage \
-    $(LOCAL_PATH)/overlay
+    $(LOCAL_PATH)/overlay \
+    $(LOCAL_PATH)/overlay-system
 
 # Vendor properties
 -include $(LOCAL_PATH)/vendor_props.mk

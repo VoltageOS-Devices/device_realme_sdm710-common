b3effb4c38d81de6a3117e13057128ada69d2d28
rk779 <rahulkrishna585@gmail.com>
2022-09-10 08:43:46 +0530

    SDM710-common: props: Don't spam logspam when bt headset is connected
    04-18 04:22:25.299 I/vendor.qti.bluetooth@1.0-ibs_handler(3040): ProcessIbsCmd: Writing IBS_WAKE_ACK
    04-18 04:22:25.579 I/vendor.qti.bluetooth@1.0-ibs_handler(3040): ProcessIbsCmd: Received IBS_SLEEP_IND: 0xFE
    04-18 04:22:25.581 I/vendor.qti.bluetooth@1.0-ibs_handler(3040): ProcessIbsCmd: Received IBS_WAKE_IND: 0xFD
    04-18 04:22:25.599 I/vendor.qti.bluetooth@1.0-ibs_handler(3040): ProcessIbsCmd: Writing IBS_WAKE_ACK
    
    Change-Id: I727827e84525a8d37da8a337ebcd0a781e4cd73a
    Signed-off-by: Cykeek <Cykeek@proton.me>

--- vendor_props.mk
+++ vendor_props.mk
@@ -52,7 +52,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Bluetooth
 PRODUCT_PROPERTY_OVERRIDES += \
-    vendor.qcom.bluetooth.soc=cherokee
+    vendor.qcom.bluetooth.soc=cherokee \
+    persist.vendor.service.bdroid.sibs=false
 
 # Camera
 PRODUCT_PROPERTY_OVERRIDES += \

1045e1446527b34e67fcbf173a3dacff87cd5b7e
Jake Weinstein <jake@aospa.co>
2022-09-10 08:39:54 +0530

    SDM710-common: Enable UXE & Prefetch OFR
    Matches stock Qualcomm configuration and improves performance.
    
    Change-Id: I75a1efb3f1963041ed4a810bffad539d81dac992
    Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com>
    Signed-off-by: Cykeek <Cykeek@proton.me>

--- sdm710.mk
+++ sdm710.mk
@@ -371,6 +371,10 @@ PRODUCT_PACKAGES += \
     libOmxVidcCommon \
     libstagefrighthw
 
+# Perf
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/perf/perf-profile0.conf:$(TARGET_COPY_OUT_VENDOR)/etc/perf/perf-profile0.conf \
+
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power-service \
